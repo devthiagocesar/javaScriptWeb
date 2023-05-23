8062cfeb394db8cc8437fc56d9662f8fcc380b66
devthiagocesar <devthiagocesar@gmail.com>
2023-05-22 22:53:33 -0300

    220523

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log ("Olá, agora estou por aqui")
+
+/*
+document -> HTML
+
+getElementById -> Trás UM elemento pelo ID
+- const input = document.getElementById ("main-input")
+getElmentByClassName -> Trás TODOS os elementos com essa classe
+- const elements = document.getElementsByClassName (paragraph-js)
+getElementByTagName -> Trás TODOS os elementos com essa TAG
+- const elements = document.getElementsByTagName ("h1")
+getElementByName -> Trás TODOS os elementos com esse Name
+- É usado em formulário
+
+quetySelector -> Trás UM elemento, o PRIMEIRO que encontrar
+querySelectorAll -> Trás TODOS os elementos que encontrar
+
+
+*/
+
+const elements = document.getElementsByTagName ("h1")
+console.log (elements)
